11f427c8a81fe4a9a54cf2560a44abc7e2e5793e
Vitor Maia <vlma@cin.ufpe.br>
2018-12-14 23:15:38 -0200

    interface elements of the problem

--- bezier.js
+++ bezier.js
@@ -28,30 +28,46 @@ class Point {
   }
 }
 
+/**
+O que falta fazer?
+• O sistema deve ser interativo, permitindo inserir, modificar e deletar os
+pontos de controle.
+• A atualização da curva é feita em tempo real.
+• O programa deve ter botões para esconder/exibir: pontos de controle,
+poligonal de controle, curva de Bézier.
+ */
+
+ // elements
 var canvas = document.getElementById('canvas');
 var drawButton = document.getElementById('drawButton');
+var toggleControlPoints = document.getElementById('toggleControlPoints');
+var toggleControlPolygon = document.getElementById('toggleControlPolygon');
+var toggleBezierCurve = document.getElementById('toggleBezierCurve');
 var clearPoints = document.getElementById('clearPoints');
+var rangeInput = document.getElementById('rangeInput');
+var polygonSizeInput = document.getElementById('polygonSize');
+var iterationsInput = document.getElementById('iterations');
+
+// user choices
+var velocity = 50;
+var polygonSize = 3;
+var iterations = 300;
+var showControlPoints = 1;
+var showControlPolygon = 1;
+var showBezierCurve = 1;
+
 var ctx = canvas.getContext('2d');
-console.log(ctx);
+console.log('Object to draw on canvas:', ctx);
 
-var controlPoints = []
-var iterations = 1000;
+var controlPoints = [];
 
-canvas.addEventListener('click', function(){
-  const x = event.offsetX;
-  const y = event.offsetY;
-  drawPoint(new Point(x, y));
-  console.log('Click on ('+x+', '+y+')')
-  controlPoints.push(new Point(x, y));
-})
-
+// Drawing on canvas:
 function drawLine(a, b) {
   ctx.beginPath();
   ctx.moveTo(a.x, a.y);
   ctx.lineTo(b.x, b.y);
   ctx.stroke();
 }
-
 function drawPoint(b) {
   ctx.moveTo(b.x, b.y);
   ctx.beginPath();
@@ -59,6 +75,8 @@ function drawPoint(b) {
   ctx.stroke();
 }
 
+
+// Bezier Stuff
 function deCasteljeu(controlPoints, t) {
   if(controlPoints.length == 1) {
     return controlPoints[0];
@@ -87,12 +105,45 @@ function drawCurve(controlPoints, iterations) {
   }
 }
 
+
+// Interface
+canvas.addEventListener('click', function(){
+  const x = event.offsetX;
+  const y = event.offsetY;
+  drawPoint(new Point(x, y));
+  console.log('Click on ('+x+', '+y+')')
+  controlPoints.push(new Point(x, y));
+})
 drawButton.addEventListener('click', function() {
   drawCurve(controlPoints, iterations);
 })
+toggleControlPoints.addEventListener('click', function() {
+  showControlPoints = 1 - showControlPoints;
+  draw();
+})
+toggleControlPolygon.addEventListener('click', function() {
+  showControlPolygon = 1 - showControlPolygon;
+  draw();
+})
+toggleBezierCurve.addEventListener('click', function() {
+  showBezierCurve = 1 - showBezierCurve;
+  draw();
+})
+
 clearPoints.addEventListener('click', function() {
   console.log('Buttons cleared')
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   controlPoints = []
 })
-
+rangeInput.addEventListener('change', function(event) {
+  console.log("Velocity is now " + event.target.value);
+  velocity = event.target.value;
+})
+polygonSizeInput.addEventListener('change', function(event){
+  console.log("Polygon size is now " + event.target.value);
+  polygonSize = event.target.value;
+})
+iterationsInput.addEventListener('change', function(event){
+  console.log("Iterations is now " + event.target.value);
+  iterations = event.target.value;
+})
